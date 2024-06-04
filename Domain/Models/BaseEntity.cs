ef810495f45e914d51addedc67fd837f6c58f8b4
Boris <brgp@live.com>
2024-06-03 19:37:03 -0600

    Add project files.

---
+++ Domain/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
