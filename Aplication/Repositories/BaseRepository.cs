dab4a92e81a770b5bf198bde7ad29122172023f1
Boris <brgp@live.com>
2024-06-04 19:21:07 -0600

    added repository

---
+++ Aplication/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ColegioDataContext _dataContext;
+        public BaseRepository(ColegioDataContext context) 
+        {
+            _dataContext = context;        
+        }
+
+        public Task<T> CreateAsync(T entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(CancellationToken cancellationToken, params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _dataContext.Set<T>().AsNoTracking().ToListAsync(cancellationToken);
+        }
+
+        public Task<T> GetById(CancellationToken cancellationToken, params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Aplication/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Aplication.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity, CancellationToken cancellationToken);
+        Task<T> GetById(CancellationToken cancellationToken, params object[] keyValues);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken);
+        Task DeleteAsync(CancellationToken cancellationToken, params object[] keyValues);
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
